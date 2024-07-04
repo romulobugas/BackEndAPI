76df96d5329a2795f82dbc997103557f1801ce4a
romulomauriz <romulo.mauriz@mobweb.net.br>
2024-07-04 19:46:02 -0300

    Adicionando sistema de verificação de usuário e senha

---
+++ Services/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿namespace BackEndAPI.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateTemporaryToken(string username);
+        bool ValidateTemporaryToken(string token);
+    }
+}
